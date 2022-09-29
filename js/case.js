58b78051c43152a9d947241aed59a5fa29f9806f
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-09-29 18:38:27 +0600

    added nothing

--- js/case.js
+++ js/case.js
@@ -1,3 +1,24 @@
+function updateCaseNumber(isIncrase) {
+    const caseField = document.getElementById('case_field');
+    const caseFieldString = caseField.value;
+    const previousCaseFieldNumber = parseInt(caseFieldString);
+
+    let newCaseFieldNumber;
+    
+    if (isIncrase === true) {
+        newCaseFieldNumber = previousCaseFieldNumber + 1;
+    } else {
+        newCaseFieldNumber = previousCaseFieldNumber - 1;
+    }
+    caseField.value = newCaseFieldNumber;
+
+    return newCaseFieldNumber;
+}
+function updateCasePrice(newCaseFieldNumber) {
+    const caseTotalPrice = newCaseFieldNumber * 59;
+    const caseTotalElement = document.getElementById('case_total_element');
+    caseTotalElement.innerText = caseTotalPrice;
+}
 
 document.getElementById('btn_case_plus').addEventListener('click', function () {
 
--- js/phone.js
+++ js/phone.js
@@ -1,7 +1,29 @@
+
+function updatePhoneNumber(isIncrase) {
+    const phoneField = document.getElementById('phone_field');
+    const phoneFieldString = phoneField.value;
+    const phoneFieldNumber = parseInt(phoneFieldString);
+    let totalPhoneElement;
+    if (isIncrase) {
+        totalPhoneElement = phoneFieldNumber + 1;
+    } else {
+        totalPhoneElement = phoneFieldNumber - 1;
+    }
+
+    phoneField.value = totalPhoneElement;
+    return totalPhoneElement ;
+}
+function totalPhonePrice(totalPhoneElement){
+    const totalPhonePrice = totalPhoneElement * 1219; 
+
+    const totalPriceElement = document.getElementById('total_phone_price');
+    totalPriceElement.innerText = totalPhonePrice; 
+}
+
+
 document.getElementById('btn_phone_plus').addEventListener('click', function () {
     const totalPhoneElement = updatePhoneNumber(true);
     totalPhonePrice(totalPhoneElement);
-
 })
 document.getElementById('btn_phone_minus').addEventListener('click', function () {
     const totalPhoneElement =  updatePhoneNumber(false);
